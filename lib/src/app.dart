621dc5a6080f4c9bf74e9f9da8686dc766502626
PMTuan <phamminhtuan@unimarmores.com>
2020-08-12 11:32:54 +0700

    update cpde

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:demo_hello_word/src/resource/loginPage.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: LoginPage(),
+    return MaterialApp(
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DEMO STREAM"),
+      ),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        child: Center(
+          child: Text(
+            "0",
+            style: TextStyle(fontSize: 30, color: Colors.black),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          click();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+  void click(){
+
   }
 }
