cae192cdc5b23c252cd8e509ad152d5d93aacb67
PMTuan <phamminhtuan@unimarmores.com>
2020-08-12 15:36:09 +0700

    update code

--- lib/src/resource/loginPage.dart
+++ lib/src/resource/loginPage.dart
@@ -1,5 +1,7 @@
 import 'package:demo_hello_word/src/blocs/login_bloc.dart';
 import 'package:demo_hello_word/src/resource/home.dart';
+import 'package:demo_hello_word/src/resource/register.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -20,115 +22,117 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-        constraints: BoxConstraints.expand(),
+        constraints: BoxConstraints.expand(), // full width, full height
         color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
-              child: Container(
-                  width: 70,
-                  height: 70,
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle, color: Color(0xffd8d8d8)),
-                  child: FlutterLogo()),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
-              child: Text(
-                "Hello\nWelcome back",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: Colors.black),
+        // singleChildScrollView scroll tren nhieu device
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 140,
               ),
-            ),
-            Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                // StreamBuilder de mapping giua bloc(stream) vs widget
-                child: StreamBuilder(
-                    stream: bloc.userStream,
-                    //builder is function
-                    builder: (context, snapshot) => TextField(
-                          controller: _userController,
-                          style: TextStyle(fontSize: 18, color: Colors.black),
-                          decoration: InputDecoration(
-                              labelText: "Username",
-                              errorText:
-                                  snapshot.hasError ? snapshot.error : null,
-                              labelStyle: TextStyle(
-                                  color: Color(0xff888888), fontSize: 15)),
-                        ))),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-              child: Stack(
-                alignment: AlignmentDirectional.centerEnd,
-                children: <Widget>[
-                  StreamBuilder(
-                      stream: bloc.passwordStream,
-                      builder: (context, snapshot) => TextField(
-                            controller: _passController,
-                            obscureText: !_showPass,
-                            style: TextStyle(fontSize: 18, color: Colors.black),
-                            decoration: InputDecoration(
-                                labelText: "Password",
-                                errorText:
-                                    snapshot.hasError ? snapshot.error : null,
-                                labelStyle: TextStyle(
-                                    color: Color(0xff888888), fontSize: 15)),
-                          )),
-                  GestureDetector(
-                    onTap: onToggleShowPass,
-                    child: Text(
-                      _showPass ? "HIDE" : "SHOW",
-                      style: TextStyle(color: Colors.blue, fontSize: 15),
+              Image.asset("ic_car_green.png"),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,40,0,6),
+                child: Text("Welcome Back !", style: TextStyle(fontSize: 22,
+                    color: Colors.black, fontWeight: FontWeight.bold),),
+              ),
+              Text("Login to continue using iCar", style: TextStyle(fontSize: 15,
+              color: Colors.grey),),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,100,0,20),
+                child: TextField(
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    prefixIcon: Container(
+                      width: 50,
+                      child: Image.asset('ic_mail.png'),
                     ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-              child: SizedBox(
-                width: double.infinity,
-                height: 56,
-                child: RaisedButton(
-                  color: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  onPressed: onSignInClicked,
-                  child: Text(
-                    "Signin",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.all(Radius.circular(6))
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-            ),
-          ],
+              TextField(
+                obscureText: true,
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: Container(
+                    width: 50,
+                    child: Image.asset('ic_lock.png'),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(6)),
+                    borderSide: BorderSide(width: 1, color: Colors.grey)
+                  ),
+                ),
+              ),
+              Container(
+                constraints: BoxConstraints.loose(Size(double.infinity, 40)),
+                //alignment can le phai
+                alignment: AlignmentDirectional.centerEnd,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Text('For got password ?',
+                  style: TextStyle(color: Colors.grey, fontSize: 16)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: RaisedButton(
+                    onPressed: _onLoginClick,
+                    child: Text("Login", style: TextStyle(color: Colors.white,
+                    fontSize: 16)),
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: RichText(
+                  text: TextSpan(
+                    text: "New user ? ",
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                    children: <TextSpan>[
+                      TextSpan(
+                        recognizer: TapGestureRecognizer()..onTap = (){
+                          _onClickRegister();
+                        },
+                        text: "Sign up for a new acount",
+                        style: TextStyle(color: Colors.blue, fontSize: 18)
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+            ],
+          ),
+
         ),
       ),
     );
   }
 
   //event click button login
-  void onSignInClicked() {
-    setState(() {
-      if (bloc.isValidInfo(_userController.text, _passController.text)) {
-        //call ham gotoHome redirect page
-        Navigator.push(context, MaterialPageRoute(builder: gotoHome));
-      }
-    });
+  void _onLoginClick() {
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));
   }
 
-  //redirect sang page home khi login thanh cong
-  Widget gotoHome(BuildContext context) {
-    return HomePage();
+  //redirect sang page register khi login thanh cong
+  void _onClickRegister(){
+    Navigator.push(context, MaterialPageRoute(builder: gotoRegister));
+  }
+  Widget gotoRegister(BuildContext context) {
+    return RegisterPage();
   }
 
   //event show/hidden password
